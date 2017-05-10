cb9578e1b593d2701db00010701d7ac6a4691e6a
c-wind <mrt@codecn.org>
2017-05-10 19:35:29 +0800

    修改sql对应字段名

--- editor/sqlhandle/handlesql.go
+++ editor/sqlhandle/handlesql.go
@@ -2,65 +2,47 @@ package sqlhandle
 
 import (
 	"encoding/base64"
-	"encoding/json"
 	"fmt"
-	"github.com/dearcode/crab/cache"
-	"github.com/dearcode/watcher/editor"
-	"github.com/dearcode/watcher/meta"
+
 	"github.com/youtube/vitess/go/sqltypes"
 	"github.com/youtube/vitess/go/vt/sqlparser"
 	"github.com/zssky/log"
+
+	"github.com/dearcode/watcher/editor"
+	"github.com/dearcode/watcher/meta"
 )
 
 var (
-	rxe *handlesqlEditor
+	hs *handlesqlEditor
 )
 
 type handlesqlEditor struct {
-	args *cache.Cache
 }
 
 func init() {
-	rxe = &handlesqlEditor{
-		args: cache.NewCache(3600),
-	}
-	editor.Register("sqlhandle", rxe)
+	editor.Register("sqlhandle", &handlesqlEditor{})
 }
 
 func (r *handlesqlEditor) Handler(msg *meta.Message, m map[string]interface{}) error {
+	sql, ok := msg.DataMap["sql"]
+	if !ok {
+		log.Infof("sql not found")
+		return nil
+	}
 
-	err := HandleSql(msg)
+	stmt, err := sqlparser.Parse(sql.(string))
 	if err != nil {
+		log.Error("解析语法树失败！！")
 		return err
 	}
-	return nil
-}
 
-func HandleSql(msg *meta.Message) error {
-
-	//jsonLog := `{"name":"mysql_rw","addr":"127.0.0.1:35636","sql":"insert into test1(ID, NAME, GENDER, AGE, CITY) values (:_ID0, :vtg2, :vtg3, :vtg4, :vtg5) /* vtgate:: keyspace_id:63c674a6c0467543 */","bindVal":{"_ID0":{"type":10262,"value":"Mg=="},"vtg1":{"type":10262,"value":"Mg=="},"vtg2":{"type":10262,"value":"d2FuZ2dhb3FpYW5n"},"vtg3":{"type":10262,"value":"55S3"},"vtg4":{"type":10262,"value":"MTk="},"vtg5":{"type":10262,"value":"5rKz5Y2X"}},"sendQueryDate":"17-4-30 20:27:49.968858588","recvResultDate":"17-4-30 20:27:49.986763601","sqlExecDuration":"17.905013ms","datanodes":[{"name":"0","tabletType":1,"idx":1,"sendDate":"17-4-30 20:27:49.968858588","recvDate":"17-4-30 20:27:49.986755798","shardExecuteTime":"17.89721ms"}]}`
-	slf := &SQLLogInfo{}
-	//somethings := []map[string]interface{}{}
-	//msg.DataMap["json_data"]=`{"sql":"insert into user(id, v, name) values (:_Id0, 2, :_name0) /* vtgate:: keyspace_id:166b40b44aba4bd6 */","bindVal":{"_Id0":1,"__seq0":1,"_name0":"bXluYW1l"},"sendQueryDate":"01-1-1 00:00:00","recvResultDate":"17-4-30 20:00:23.724694493","sqlExecDuration":"2562047h47m16.854775807s","datanodes":[{"name":"-20","tabletType":1,"idx":1,"sendDate":"01-1-1 00:00:00","recvDate":"17-4-30 20:00:23.724691915","shardExecuteTime":"2562047h47m16.854775807s"}]}`
-	//aa:=`{"sql":"insert into user(id, v, name) values (:_Id0, 2, :_name0) /* vtgate:: keyspace_id:166b40b44aba4bd6 */","bindVal":{"_Id0":1,"__seq0":1,"_name0":"bXluYW1l"},"sendQueryDate":"01-1-1 00:00:00","recvResultDate":"17-4-30 20:00:23.724694493","sqlExecDuration":"2562047h47m16.854775807s","datanodes":[{"name":"-20","tabletType":1,"idx":1,"sendDate":"01-1-1 00:00:00","recvDate":"17-4-30 20:00:23.724691915","shardExecuteTime":"2562047h47m16.854775807s"}]}`
-	json_data := fmt.Sprint(msg.DataMap["json_data"])
-	err := json.Unmarshal([]byte(json_data), &slf)
-	if err != nil {
-		log.Errorf("%v", err)
-		return err
-	} else {
-		stmt, err := sqlparser.Parse(slf.SQL)
-		if err != nil {
-			log.Error("解析语法树失败！！")
-			return err
-		}
-		UnNormalize(stmt, slf.BindVal)
-		out := sqlparser.String(stmt)
-		fmt.Println(out)
-
+	if bv, ok := msg.DataMap["bindVal"]; ok {
+		UnNormalize(stmt, bv.(map[string]interface{}))
 	}
-	msg.DataMap["sql"] = slf.SQL
-	return sql(msg)
+
+	log.Debugf("stmt:%v", sqlparser.String(stmt))
+
+	return analysis(msg)
 
 }
 
@@ -100,8 +82,8 @@ func UnNormalize(stmt sqlparser.Statement, bindVals map[string]interface{}) {
 	}, stmt)
 }
 
-// sql 通过语法树分析sql的类型，表明，条件
-func sql(msg *meta.Message) error {
+// analysis 通过语法树分析sql的类型，表明，条件
+func analysis(msg *meta.Message) error {
 	sql := msg.DataMap["sql"].(string)
 	tree, err := sqlparser.Parse(sql)
 	if err != nil {
